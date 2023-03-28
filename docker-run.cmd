0b9c9225f8b3ae6d3dc730fc55ae8d141280f891
ikenchoco <105382400+ikenchoco@users.noreply.github.com>
2023-03-29 02:05:08 +0900

    Delete docker-run.cmd

--- docker-run.cmd
+++
@@ -1,5 +0,0 @@
-docker run --rm -it -p 8080:8080 ^
-    -e CONFIGFILE=./config.json ^
-    -e ISSUANCEFILE=./issuance_request_config.json ^
-    -e PRESENTATIONFILE=./presentation_request_config.json ^
-    node-aadvc-api-idtokenhint:latest
